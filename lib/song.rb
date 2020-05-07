3853618286dd57be1cb81d96153c39c2ab92b2c6
Matt Im <matthew.seiim@gmail.com>
2020-05-07 17:32:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,6 +44,7 @@ attr_accessor :name, :artist, :genre
       if artist_count[artist]
         artist_count[artist]+=1 
       else artist_count[artist] = 1 
+      end
     end
     artist_count
     end
