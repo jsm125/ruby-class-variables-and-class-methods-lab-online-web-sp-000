4232d020a2dc218c7aa7d0390c290fc010e85b97
Matt Im <matthew.seiim@gmail.com>
2020-05-07 15:39:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,9 +24,8 @@ attr_accessor :name, :artist, :genre
   
   def self.artists(artist) #Shows all of the artists of the existing songs - unique only
     if @@artists.include?("artist") == true 
-    else
-    @@artists << artist   
   end
+    @@artists << artist   
     @@artists
   end
   
