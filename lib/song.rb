0261eb36fc56a76f8ee173e43109e22d03802691
Matt Im <matthew.seiim@gmail.com>
2020-05-07 15:33:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,7 @@ attr_accessor :name, :artist, :genre
     if @@artists.include?(artist) == true 
       end
     @@artists << artist   
-    self.artist 
+    @@artist 
   end
   
   def self.genre_count #Show us the number of songs for each genre / Add to genre_count
