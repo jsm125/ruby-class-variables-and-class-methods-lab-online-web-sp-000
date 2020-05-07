d9d2219c5984b0b09dbc3691520faa9774c6a4f3
Matt Im <matthew.seiim@gmail.com>
2020-05-07 17:33:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ attr_accessor :name, :artist, :genre
   
   def self.artist_count #Show us the number of artists for each song/Add to artist_count
     artist_count = {}
-    @@artists.each do |artists|
+    @@artists.each do |artist|
       if artist_count[artist]
         artist_count[artist]+=1 
       else artist_count[artist] = 1 
